a65d98fa0b903e8dbc3473c0a5761013d4731611
JoniMeichtry11 <joni.meichtry@gmail.com>
2021-05-25 23:05:43 -0300

    ahora si es el final

---
+++ src/app/componentes/cebecero/cebecero.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cebecero',
+  templateUrl: './cebecero.component.html',
+  styleUrls: ['./cebecero.component.css']
+})
+export class CebeceroComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
